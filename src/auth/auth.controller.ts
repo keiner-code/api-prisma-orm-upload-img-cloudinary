96711fb94a23a3a096bc4ba50b320fb34df48809
keiner peñaranda <keinerjpp@gmail.com>
2024-07-06 21:11:09 -0500

    add endpoint check Token user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
+  private user: User;
   constructor(private authService: AuthService) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
   login(@Req() req: any) {
-    const user = req.user as User;
-    return this.authService.generateJWT(user);
+    this.user = req.user as User;
+    return this.authService.generateJWT(this.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('user/check')
+  checkAuth() {
+    return this.authService.generateJWT(this.user);
   }
 }
